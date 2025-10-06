84dc0d47ab93a42cfee328d659dfda6efc237eea
thousif <thousif7896@gmail.com>
2025-10-06 20:27:06 +0530

    feat: implemented Task 3 - User, Entity and Repository

---
+++ src/main/java/com/thousif/trading/enums/Role.java
@@ -0,0 +1,6 @@
+package com.thousif.trading.enums;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/thousif/trading/enums/UserStatus.java
@@ -0,0 +1,7 @@
+package com.thousif.trading.enums;
+
+public enum UserStatus {
+    ACTIVE,
+    SUSPENDED,
+    INACTIVE
+}
