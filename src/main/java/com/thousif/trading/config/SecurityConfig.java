76f532870513ca0c5bd8d41bc8cf0857d4720ce3
thousif <thousif7896@gmail.com>
2025-10-09 04:09:34 +0530

    feat: implemented Task 8 - Create Authentication Controller

--- src/main/java/com/thousif/trading/config/SecurityConfig.java
+++ src/main/java/com/thousif/trading/config/SecurityConfig.java
@@ -27,6 +27,7 @@ public class SecurityConfig {
     private final CustomUserDetailsService userDetailsService;
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session
@@ -35,8 +36,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/**", "/api/public/**").permitAll()
                         .anyRequest().authenticated())
                 .authenticationProvider(authenticationProvider())
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .build();
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
